ddfbf5aee4da198c3dfa3ae3085c89798cc85d44
PAR-iTY <44795502+PAR-iTY@users.noreply.github.com>
2021-08-22 20:34:55 +1200

    static public facing jovid map files

---
+++ init.sh
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------#
+#                                                                      #
+#     DO SOME DUAL-GIT REPO MAGIC TO UPDATE...OR fetch(upstream) ??    #
+#                                                                      #
+# ---------------------------------------------------------------------#
+
+git init
+
+# automatically adds git remote add origin 
+# git clone https://github.com/PAR-iTY/nz-jovid-data.git
+
+# but use this instead so my local changes are preserved
+git remote add origin https://github.com/PAR-iTY/nz-jovid-data.git
