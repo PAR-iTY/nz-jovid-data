8159a393f312c10230c3f2917e3b136f750f9103
PAR-iTY <44795502+PAR-iTY@users.noreply.github.com>
2021-09-01 03:22:32 +1200

    experimenting with property picking and linking location events by date/time

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 // ------------------------------------------------------------------- //
 
+/*
+ * todo:
+ *  - improve reduce accObj so it collects more information per city
+ *  - seperate data-cleaning functionality from chart functionality
+ *  - and/or develop a new data structure e.g. for time-series line chart
+ *  - babel this script for compatability - improve async/lazyness
+ *  - improve UX - site title/heading, explain covid-chart & jovid-map
+ */
+
+// ------------------------------------------------------------------- //
+
 // goal: lazy/async load both map and table
 // this is a good mini-test for async modular loading
 // good for general perf and also component-based web design
 window.addEventListener('DOMContentLoaded', async e => {
-  console.log('DOMContentLoaded:', e.timeStamp);
+  // console.log('DOMContentLoaded:', Math.(e.timeStamp / 1000));
+  console.log(
+    `DOMContentLoaded in ${
+      Math.round((e.timeStamp + Number.EPSILON * 100) / 100) / 1000
+    } seconds`
+  );
 
   const locationsURL =
     'https://raw.githubusercontent.com/minhealthnz/nz-covid-data/main/locations-of-interest/august-2021/locations-of-interest.geojson';
 
   const locations = await fetchJSON(locationsURL);
 
-  initTable(locations);
+  initChart(locations);
   initMap(locations);
 });
 
@@ -29,28 +45,91 @@ const fetchJSON = async url => {
 
 // ------------------------------------------------------------------- //
 
-const initTable = data => {
-  // use reduce to new object to do all data transformation in one pass (!)
+const getDate = value => {
+  // convert to MM/DD/YYYY format for new Date(input)
+  const dateStr = `${value[3]}${value[4]}/${value[0]}${
+    value[1]
+  }/${value.substring(6, 10)}`;
+
+  // console.log('dateStr:', dateStr);
+  return new Date(dateStr);
+  // console.log(dateObj.toLocaleDateString());
+
+  // prettify date for display to users
+  // const date = value.substring(0, 10);
+  // const time = value.substring(12);
+  // console.log('date: ', date);
+  // console.log('time: ', time);
+  // return [date, time];
+};
+
+// what else to do with the data...
+// i could create a sequence starting from the first recorded location to the last!
+
+const initChart = data => {
+  const seqData = data.features
+    .map(feature => {
+      const { Start, End, id } = feature.properties;
+      // console.log('start:', getDate(Start));
+      const x = { Start, End, id };
+      return x;
+    })
+    .sort((a, b) => getDate(a.Start) - getDate(b.Start));
+
+  // ^ needs to be more complex to link the trail of locations
+  // must detect: first starting location's end time
+  //              next starting location's start time
+
+  // so this needs to do a look-ahead?
+
+  // i can make a crude assumption: it matters less what the end time is
+  // because the data shows when (someone) was at a location, and i will
+  // show the relationship between that contagious event and the next.
+
+  // because this data does not identify citizens (thank Wanye @ Stats)
+  // i should not pretend that the sequence is linear.
+  // every start time event should be linked with a line to every soon
+  // after event (give a param of 3 hours [or a day, depends on data])
+
+  // strongest lines should be ranked by a few criteria:
+  // how close geographically they are
+  // how closely in start times they are
+  // how close the firsts end time is to the nexts start time
+
+  console.log('seqData: ', seqData);
+
+  // use reduce to new object to do data transformation and aggregation in one pass
   const cityData = data.features.reduce((accObj, feature) => {
-    // loop over feature properties by key
-    Object.keys(feature.properties).forEach(key => {
+    // outside the entries loop, can I aggregate data in a different way??
+
+    // here i could look at the entire properties object...
+    // const { Added, City, Start, End, Event, Location } = feature.properties;
+    // accObj.feature = { Added, City, Start, End, Event, Location };
+
+    // loop over feature properties
+    for (const [key, value] of Object.entries(feature.properties)) {
       // detect missing values
-      if (key !== 'Added' && !feature.properties[key]) {
+      if (key !== 'Added' && !value) {
         console.warn(
           'feature has a non-trivial missing value:',
           feature.properties
         );
       }
 
+      // [rest of code moved to getDate]
+      // add date data to accumulator object
+      // accObj[date] = (accObj[date] || 0) + 1;
+      // }
+
       // accumulate/count features by city
       // initialise to 0 when accumulator starts
       // use property key to match city
-      // and to assign city name as accObj key
+      // use value (city name) as accObj key
       if (key && key === 'City') {
-        accObj[feature.properties[key]] =
-          (accObj[feature.properties[key]] || 0) + 1;
+        // add city value to accumulator object
+        accObj[value] = (accObj[value] || 0) + 1;
       }
-    });
+    }
     return accObj;
   }, {});
 
@@ -166,7 +245,6 @@ const initMap = data => {
     strokeWidth: 3,
     markerSymbol: 'C' // ☢
   });
-  viewer.dataSources.add(locations);
 
   const jocations = Cesium.GeoJsonDataSource.load(
     './assets/geojson/jocations-of-interest.geojson',
@@ -179,7 +257,9 @@ const initMap = data => {
       markerSymbol: 'J'
     }
   );
+
   viewer.dataSources.add(jocations);
+  viewer.dataSources.add(locations);
 
   viewer.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(174.641911, -36.949, 15000.0),
